30deffb64dc5877f19af71effc91f17ed6f4866d
Jackie <lcd.jackson@gmail.com>
2023-11-12 17:05:14 +0800

    initial commit -> assignment 5.4

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Battery from 'expo-battery';
 
 export default function App() {
+  const [batteryLevel, setBatteryLevel] = useState(null);
+
+  useEffect(() => {
+    const getBatteryLevel = async () => {
+      const batteryInfo = await Battery.getBatteryLevelAsync();
+      const level = (batteryInfo * 100); 
+      setBatteryLevel(level);
+    };
+
+    getBatteryLevel();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>
+        Battery Level: {batteryLevel !== null ? `\n${batteryLevel.toFixed(2)}%` : 'Loading...'}
+      </Text>
     </View>
   );
 }
@@ -13,8 +27,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: 'green',
+    fontSize: 30,
+    marginVertical: 5,
   },
 });
